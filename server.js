b5cc83ff29b1fb8caec3e9b6a5dff9cdd5d80597
Ashwini Sharma <83415972+ashwini286@users.noreply.github.com>
2025-03-01 15:22:06 +0530

    update cors

--- server.js
+++ server.js
@@ -15,10 +15,19 @@ connectDB();
 const app = express();
 app.use(express.json());
 
-const allowedOrigins = ["*"];
+const allowedOrigins = [
+    "http://localhost:3000",
+    "https://kanban-board-version2.netlify.app"
+];
+
 app.use(cors({
-    // origin:  allowedOrigins, // Allow requests from frontend
-    origin: "http://localhost:3000", // Allow requests from frontend
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);  // Allow request
+        } else {
+            callback(new Error("Not allowed by CORS"));  // Block request
+        }
+    },
     methods: "GET,POST,PUT,DELETE",
     credentials: true
 }));
