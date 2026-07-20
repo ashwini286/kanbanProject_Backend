2bcbbe1fe4a19e39a1d3c61161064f8de3df6028
ashwini kuamri <ashwini.swe@gmail.com>
2026-07-20 19:40:54 +0530

    fix: add authMiddleware to reorder route so user is passed to activity log

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -1,11 +1,12 @@
 import express from "express";
 import { createTask, getTasks, updateTask, deleteTask, reorderTasks, addAttachment, deleteAttachment } from "../controllers/taskController.js";
 import { upload } from "../middlewares/uploadMiddleware.js";
+import { authMiddleware } from "../middlewares/authMiddleware.js";
 const router = express.Router();
 
 router.post("/", createTask);
 router.get("/column/:columnId", getTasks);
-router.put("/reorder/bulk", reorderTasks);
+router.put("/reorder/bulk", authMiddleware, reorderTasks);
 router.put("/:id", updateTask);
 router.delete("/:id", deleteTask);
 
